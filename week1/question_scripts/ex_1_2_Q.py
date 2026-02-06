75fcb677d70ed63059e9dae880f9f02a10a18892
lelegod <kylenathanya@gmail.com>
2026-02-06 18:13:04 +0100

    finished part 1 and 2

--- week1/question_scripts/ex_1_2_Q.py
+++ week1/question_scripts/ex_1_2_Q.py
@@ -15,7 +15,8 @@ from sklearn.metrics import mean_squared_error
 
 # --- SECTION 1: Parameters ---
 np.random.seed(42)
-n_samples = 100
+n_train = 100
+n_test = 500
 n_simulations = 500
 sigma = 1.0          # Noise level
 beta_true = np.array([2, 0])
@@ -26,7 +27,8 @@ lambdas = np.logspace(-2, 5, 25)
 # Fixed X for consistent theoretical/empirical comparison
 mean = [0, 0]
 cov = [[1, 0.98], [0.98, 1]] # High correlation
-X_fixed = np.random.multivariate_normal(mean, cov, n_samples)
+X_train = np.random.multivariate_normal(mean, cov, n_train)
+X_test = np.random.multivariate_normal(mean, cov, n_test)
 
 # --- SECTION 2: Simulation ---
 # TASK: Iterate through lambdas and simulations.
@@ -37,6 +39,34 @@ X_fixed = np.random.multivariate_normal(mean, cov, n_samples)
 
 bias_sq = []
 variance = []
+mean_train_mse = []
+mean_test_mse = []
+
+for l in lambdas:
+    all_preds = []
+    train_mse = []
+    test_mse = []
+
+    for _ in range(n_simulations):
+        y_train = X_train @ beta_true + np.random.normal(0, sigma, n_train)
+        y_test = X_test @ beta_true + np.random.normal(0, sigma, n_test)
+
+        model = Ridge(alpha=l).fit(X_train, y_train)
+
+        all_preds.append(model.predict(x_eval)[0])
+        train_preds = model.predict(X_train)
+        test_preds = model.predict(X_test)
+
+        train_mse.append(mean_squared_error(y_train, train_preds))
+        test_mse.append(mean_squared_error(y_test, test_preds))
+
+    bias_sq.append((np.mean(all_preds) - target_val)**2)
+    variance.append(np.var(all_preds))
+    mean_train_mse.append(np.mean(train_mse))
+    mean_test_mse.append(np.mean(test_mse))
+
+bias_sq = np.array(bias_sq)
+variance = np.array(variance)
 
 print('Simulating Ridge complexity sweep...')
 # YOUR CODE HERE
@@ -46,8 +76,17 @@ print('Simulating Ridge complexity sweep...')
 # TASK: Plot Bias^2, Variance, and Total (Bias^2 + Var) vs Lambdas.
 # Remember to invert the x-axis for complexity.
 
-# plt.figure(figsize=(10, 6))
-# ...
-# plt.xscale('log')
-# plt.gca().invert_xaxis()
-# plt.show()
+plt.figure(figsize=(10, 6))
+plt.plot(lambdas, bias_sq, label=r'$Bias^2$', color='green', linestyle='--')
+plt.plot(lambdas, variance, label='Variance', color='orange', linestyle='--')
+plt.axhline(sigma**2, label=r'Noise $\rho^2$', color='gray', linestyle=':')
+plt.plot(lambdas, mean_train_mse, label='Mean Train MSE', color='blue')
+plt.plot(lambdas, mean_test_mse, label='Mean Test MSE', color='red')
+plt.axvline(lambdas[np.argmin(mean_test_mse)], label='Sweet Spot', linestyle='-.', color='black')
+plt.xscale('log')
+plt.gca().invert_xaxis()
+plt.legend()
+plt.xlabel(r'lambda $\lambda$')
+plt.ylabel('Error')
+plt.tight_layout()
+plt.show()
